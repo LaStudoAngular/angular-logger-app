5f472e290e121946a8b4c64e4f62f55ee0a011d6
Valery Semenenko <gearmobile@gmail.com>
2019-07-25 09:26:44 +0300

    feat: add moment.js; styling logger-list component

--- src/app/components/logger-list/logger-list.component.ts
+++ src/app/components/logger-list/logger-list.component.ts
@@ -1,24 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import {LogModel} from '../../models/log.model';
+import { LogModel } from '../../models/log.model';
 import * as faker from 'faker';
+import * as moment from 'moment';
 
 @Component({
   selector: 'dl-logger-list',
   templateUrl: './logger-list.component.html',
-  styleUrls: ['./logger-list.component.scss']
+  styleUrls: ['./logger-list.component.scss'],
 })
 export class LoggerListComponent implements OnInit {
   logs: LogModel[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     this.logs = [
-      new LogModel(faker.random.uuid(), faker.lorem.sentence(), faker.date.recent()),
-      new LogModel(faker.random.uuid(), faker.lorem.sentence(), faker.date.recent()),
-      new LogModel(faker.random.uuid(), faker.lorem.sentence(), faker.date.recent()),
-      new LogModel(faker.random.uuid(), faker.lorem.sentence(), faker.date.recent())
+      new LogModel(
+        faker.random.uuid(),
+        faker.lorem.words(5),
+        moment(faker.date.recent()).format('DD/MM/YYYY HH:MM:SS'),
+      ),
+      new LogModel(
+        faker.random.uuid(),
+        faker.lorem.words(5),
+        moment(faker.date.recent()).format('DD/MM/YYYY HH:MM:SS'),
+      ),
+      new LogModel(
+        faker.random.uuid(),
+        faker.lorem.words(5),
+        moment(faker.date.recent()).format('DD/MM/YYYY HH:MM:SS'),
+      ),
+      new LogModel(
+        faker.random.uuid(),
+        faker.lorem.words(5),
+        moment(faker.date.recent()).format('DD/MM/YYYY HH:MM:SS'),
+      ),
     ];
   }
 
+  trackByFunction(item): string {
+    return item ? item.id : undefined;
+  }
 }
